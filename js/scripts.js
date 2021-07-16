6a162af73f81eb93fcce5b1e9248b83599e81dfe
Dylan-Allison-Damitz <dylandamitz@gmail.com>
2021-07-16 13:49:21 -0700

    changed pieSize method to totalPrice

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,23 @@ function Pizza() {
   this.price = 0;
 }
 
-Pizza.prototype.pieSize = function(size) {
-if(this.size === "Children") {
-  (this.price += 12);
-} else if(this.size === "Personal") {
-  (this.price += 20);
-} else if(this.size === "DateNight") {
-  (this.price += 50);
+Pizza.prototype.totalPrice = function(size) {
+  if(this.size === "Children") {
+  this.price += 12;
+} if(this.size === "Personal") {
+  this.price += 20;
+} if(this.size === "DateNight") {
+  this.price += 50;
+} if(this.toppings === "Bob") {
+  this.price += 3;
+} if(this.toppings === "Junebug") {
+  this.price += 5;
+} if(this.toppings === "Bocephus") {
+  this.price += 7;
   }
-  return console.log(this.price)
+  return this.price
+}
+
+  
+  
 };
