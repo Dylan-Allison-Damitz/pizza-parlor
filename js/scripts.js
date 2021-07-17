0565d2d71748b45341b0b119fabc7c02423d23d7
Dylan-Allison-Damitz <dylandamitz@gmail.com>
2021-07-17 14:59:08 -0700

    add functional UI logic that returns proper values

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,26 @@
 // Business Logic
 
-function Pizza() {
-  this.toppings = {};
-  this.size = {};
+function Pizza(size, toppings) {
+  this.toppings = toppings;
+  this.size = size;
   this.price = 0;
-}
+};
 
-Pizza.prototype.totalPrice = function() {
-    if(this.size === "Children") {
-    this.price += 12;
-    } else if(this.size === "Personal") {
-    this.price += 20;
-    } else if(this.size === "DateNight") {
-    this.price += 50;
-    } 
-    if(this.toppings === "Bob") {
-    this.price += 3;
-    } ;
-    if(this.toppings === "Junebug") {
-    this.price += 5;
-    }; 
-    if(this.toppings === "Bocephus") {
-    this.price += 7;
+Pizza.prototype.sizePrice = function(size) {
+    if(this.size == "Children") {
+      this.price +=12;
+    } else if(this.size == "Personal") {
+      this.price +=20;
+    } else if(this.size == "DateNight") {
+      this.price +=50;
+    }
+    
+    if(this.toppings == "Bob") {
+      this.price += 3;
+    } else if(this.toppings == "Junebug") {
+      this.price +=3;
+    } else if(this.toppings == "Bocephus") {
+      this.price += 7;
     };
   return this.price;
 };
@@ -33,10 +32,10 @@ $(document).ready(function() {
     event.preventDefault();
     let userSize = $("input:radio[name=size]:checked").val();
     let userToppings = $("input:radio[name=toppings]:checked").val();
-    let pizzaOrder = userSize + userToppings
-    let userPizza = new Pizza(pizzaOrder);
-    $("#output").text(userPizza.totalPrice());
     
+    const userPizza = new Pizza(userSize, userToppings);
+    userPizza.sizePrice()
+    $("#output").text(userPizza.sizePrice());
   });
 });
 
