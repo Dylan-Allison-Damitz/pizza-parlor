4e46e0d8ccb5739535452730de7f021825ffff4b
Dylan-Allison-Damitz <dylandamitz@gmail.com>
2021-07-17 10:46:34 -0700

    add UI logic that returns this.price

--- js/scripts.js
+++ js/scripts.js
@@ -6,30 +6,38 @@ function Pizza() {
   this.price = 0;
 }
 
-Pizza.prototype.totalPrice = function(size) {
-  if(this.size === "Children") {
-  this.price += 12;
-} if(this.size === "Personal") {
-  this.price += 20;
-} if(this.size === "DateNight") {
-  this.price += 50;
-} if(this.toppings === "Bob") {
-  this.price += 3;
-} if(this.toppings === "Junebug") {
-  this.price += 5;
-} if(this.toppings === "Bocephus") {
-  this.price += 7;
-  }
-  return this.price
-}
+Pizza.prototype.totalPrice = function() {
+    if(this.size === "Children") {
+    this.price += 12;
+    } else if(this.size === "Personal") {
+    this.price += 20;
+    } else if(this.size === "DateNight") {
+    this.price += 50;
+    } 
+    if(this.toppings === "Bob") {
+    this.price += 3;
+    } ;
+    if(this.toppings === "Junebug") {
+    this.price += 5;
+    }; 
+    if(this.toppings === "Bocephus") {
+    this.price += 7;
+    };
+  return this.price;
+};
 
 // UI Logic
-let completePizza = new Pizza (userToppings, userSize);
 
 $(document).ready(function() {
   $("form#formOne").submit(function(event) {
     event.preventDefault();
-    const userToppings = $("input:radio[name=toppings]:checked").val();
-    const userSize = $("input:radio[name=size]:checked").val();
+    let userSize = $("input:radio[name=size]:checked").val();
+    let userToppings = $("input:radio[name=toppings]:checked").val();
+    let pizzaOrder = userSize + userToppings
+    let userPizza = new Pizza(pizzaOrder);
+    $("#output").text(userPizza.totalPrice());
+    
   });
-})
+});
+
+    
